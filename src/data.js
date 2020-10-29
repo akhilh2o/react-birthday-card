f5a234b884ba211f95397bb708d96694f5308df3
akhilh2o <guptaakhilesh978@gmail.com>
2020-10-29 23:45:13 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: 'Akhilesh',
+    age: 23,
+    image: 'https://instagram.fknu1-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.92.736.736a/s240x240/90793306_2600653490258867_7058480438802039828_n.jpg?_nc_ht=instagram.fknu1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=RmUkEclboJoAX8ERwBE&tp=16&oh=c44e0c7140160937201ab3770b4c184d&oe=5FC2ECD9',
+  },
+  {
+    id: 2,
+    name: 'Bertie Yates',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 3,
+    name: 'Hester Hogan',
+    age: 32,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 4,
+    name: 'Larry Little',
+    age: 36,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 5,
+    name: 'Sean Walsh',
+    age: 34,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 6,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+
+];
